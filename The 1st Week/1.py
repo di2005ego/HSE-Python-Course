4c091f50db44f3e55f2bd0106396e596c073b843
di.ego <fbi@gmail.com>
2024-09-22 16:08:03 +0300

    LeetCode Changes

--- The 1st Week/1.py
+++ The 1st Week/1.py
@@ -13,3 +13,22 @@ for r in range(len(s)):
     counter = max(counter, r - l + 1)
 lengthOfLongestSubstring = counter
 print(lengthOfLongestSubstring)
+
+'''
+Решение в LeetCode
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        finalCounter = 0
+        l = 0
+        arr = set()
+        for r in range(len(s)):
+            rChar = s[r]
+            while rChar in arr:
+                lChar = s[l]
+                arr.remove(lChar)
+                l += 1
+            arr.add(rChar)
+            finalCounter = max(finalCounter, r - l + 1)
+        lengthOfLongestSubstring = finalCounter
+        return lengthOfLongestSubstring
+'''
--- The 1st Week/3.py
+++ The 1st Week/3.py
@@ -12,3 +12,20 @@ for i in range (0, numRows):
         if i != 0 and i != numRows - 1 and (j + step - 2*i) < n:
             res+=s[j + step - 2*i]
 print(res)
+
+'''
+Решение в LeetCode
+class Solution(object):
+    def convert(self, s, numRows):
+        n = len(s)
+        if numRows == 1:
+            return s
+        res = ""
+        step = 2*numRows-2
+        for i in range (0, numRows):
+            for j in range (i, len(s), step):
+                res += s[j]
+                if i != 0 and i != numRows - 1 and (j + step - 2*i) < n:
+                    res+=s[j + step - 2*i]
+        return res
+'''
